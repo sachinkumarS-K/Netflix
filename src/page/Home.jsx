43d43b5631e98fb4def9c274198a01c9c13fb4a4
sachinkumarS-K <sachinraj.s.r.123456@gmail.com>
2024-01-01 08:55:40 +0530

    update

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,5 @@
 import React, { useEffect } from 'react'
-import Header from '../components/Header'
 import Login from '../components/login'
-
- import { auth } from '../utils/firebase';
-import { useDispatch } from 'react-redux';
-import { addUser, removeUser } from '../redux/userSlice';
 
 const Home = () => {
  
